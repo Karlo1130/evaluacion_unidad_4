0f4114e2e00d71e3c14d4443b784da13d2779959
karlo <kaes_21@alu.uabcs.mx>
2024-11-06 22:21:28 -0700

    forgot password agregada (sin devolucion de contrasenna)

--- app/authController.php
+++ app/authController.php
@@ -30,6 +30,14 @@
                 $authController->registerUser($name, $lastname, $email, $phone_number, $created_by, $role, $password, $profile_photo_file);
 
                 break;
+
+            case 'forgotPassword':
+
+                    $email = $_POST['email'];
+
+                    $authController->forgotPassword($email);
+
+                break;
             
             default:
                 # code...
@@ -104,19 +112,63 @@
             curl_close($curl);
             
             $response = json_decode($response, false);
-            echo $response;
 
-            if(isset($response) && $response['code'] == 4){
+            if(isset($response) && $response->code == 4){
                 $_SESSION['message_type'] = "success";
             } else {
                 $_SESSION['message_type'] = "error";
             }
             $_SESSION['message'] = $response->message;
+
+            //TODO: cambiar la redireccion a al pantalla correspondiente
             header("Location:  home");
 
         }
 
-        public function forgotPassword() {
+        //! respuesta a peticiones rara
+        //? las respuestas tienen sentido?
+        public function forgotPassword($email) {
+
+            $curl = curl_init();
+
+            curl_setopt_array($curl, array(
+            CURLOPT_URL => 'https://crud.jonathansoto.mx/api/forgot-password',
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'POST',
+            CURLOPT_POSTFIELDS => array('email' => $email),
+            CURLOPT_HTTPHEADER => array(
+                'Cookie: XSRF-TOKEN=eyJpdiI6IkpoS2w1ZDdzYXByZ3AxM1E3SFBCaFE9PSIsInZhbHVlIjoiQktUK3p4VEtjL0NBWWFvamg0UEovcVBXVHNPQXJnNndmejZmLzdCdENtdHpGMDJJbUQvT1NVdWV0Q2tZbHd5YlY3bkxUWnlDMDVPVVp6Z2dnYUlnOGlnVUlmWVd2UllqcGwvaWQ1aTZlTUpMbFdvWWFJc3RaV0d4SkdVN1MzdU0iLCJtYWMiOiIzZTU3MGU1MWMyMDkwM2FkMDcxNGFhZWRkMjg2NTMwYWRhZmYyNGRhNmQ4MTIzYTFiMjhkM2Q3OTFkNzcxMTU4IiwidGFnIjoiIn0%3D; apicrud_session=eyJpdiI6IlFOd1kzUDVjTllQOWFMdGx4SWg1bkE9PSIsInZhbHVlIjoiVjlBelpremdkWGNBZU80VkxTVkJtMnR1dFkxVVhNN3FlZE9BRXNFT2tvQ056dUVJNXZRdXhoSGtEenpHcmJ6RXh3U3B4ZE5MT0xZYVFaNWFndk9VMEhZWlFRR2llMzlRbC9ibUJjUXJvdVdOanhtUldmMkIyNHBOK0ZMZEdZR0QiLCJtYWMiOiIyYjU5MWQxZTFiZDU1MzA3MWFmYmNkOGRmOTIzNGJjZDc5NzljMGE0OGRlMjk5OGJkMGE3NWI5OTBmNzliMjYwIiwidGFnIjoiIn0%3D'
+            ),
+            ));
+
+            $response = curl_exec($curl);
+
+            curl_close($curl);
+            
+            $response = json_decode($response, false);
+            var_dump($response);
+
+            if(isset($response) && $response->code == 4){
+                $_SESSION['message_type'] = "success";
+                $_SESSION['message'] = $response->message;
+                //TODO: enviar a front la contrasena del usuario
+                //*la unica respuesta de la api es passswords.throttled si es un email correct y user si es un email inexistente
+            } else {
+                //* solo se ejecuta si response es null (no contiene mensaje)
+                $_SESSION['message_type'] = "error";
+                $_SESSION['message'] = "No se obtuvo una respuesta";
+                //TODO: cambiar la redireccion a al pantalla correspondiente
+                header("Location:  home");
+                exit;
+            }
+
+            //TODO: cambiar la redireccion a al pantalla correspondiente
+            header("Location:  home");
 
         }
 
