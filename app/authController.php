84b59bfb032c33d4fc441ef568266511147ec45f
karlo <kaes_21@alu.uabcs.mx>
2024-11-07 18:35:39 -0700

    logout agregado y session data en controlador

--- app/authController.php
+++ app/authController.php
@@ -1,5 +1,9 @@
 <?php
 
+    if (session_status() === PHP_SESSION_NONE) {
+        session_start();
+    }
+
     include 'tokenGenerator.php';
 
     if(isset($_POST['access'])){
@@ -35,7 +39,18 @@
 
                     $email = $_POST['email'];
 
+                    echo $email;
                     $authController->forgotPassword($email);
+
+                break;
+            
+            case 'logout':
+
+                    $email = $_POST['email'];
+
+                    echo $email;
+
+                    $authController->logout($email);
 
                 break;
             
@@ -75,8 +90,6 @@
 
                 $data = $response->data;
 
-                session_start();
-
                 $_SESSION['data'] = $data;
                 $_SESSION['id'] = $data->id;
                 $_SESSION['global_token'] = $global_token;
@@ -85,6 +98,7 @@
                 header("Location: home");
             } else {
                 $_SESSION['message_type'] = "error";
+                header("Location: login");
             }
             $_SESSION['message'] = $response->message;
 
@@ -129,6 +143,8 @@
         //? las respuestas tienen sentido?
         public function forgotPassword($email) {
 
+            $data = $_SESSION['data'];
+
             $curl = curl_init();
 
             curl_setopt_array($curl, array(
@@ -142,6 +158,7 @@
             CURLOPT_CUSTOMREQUEST => 'POST',
             CURLOPT_POSTFIELDS => array('email' => $email),
             CURLOPT_HTTPHEADER => array(
+                'Authorization: '. $data->token,
                 'Cookie: XSRF-TOKEN=eyJpdiI6IkpoS2w1ZDdzYXByZ3AxM1E3SFBCaFE9PSIsInZhbHVlIjoiQktUK3p4VEtjL0NBWWFvamg0UEovcVBXVHNPQXJnNndmejZmLzdCdENtdHpGMDJJbUQvT1NVdWV0Q2tZbHd5YlY3bkxUWnlDMDVPVVp6Z2dnYUlnOGlnVUlmWVd2UllqcGwvaWQ1aTZlTUpMbFdvWWFJc3RaV0d4SkdVN1MzdU0iLCJtYWMiOiIzZTU3MGU1MWMyMDkwM2FkMDcxNGFhZWRkMjg2NTMwYWRhZmYyNGRhNmQ4MTIzYTFiMjhkM2Q3OTFkNzcxMTU4IiwidGFnIjoiIn0%3D; apicrud_session=eyJpdiI6IlFOd1kzUDVjTllQOWFMdGx4SWg1bkE9PSIsInZhbHVlIjoiVjlBelpremdkWGNBZU80VkxTVkJtMnR1dFkxVVhNN3FlZE9BRXNFT2tvQ056dUVJNXZRdXhoSGtEenpHcmJ6RXh3U3B4ZE5MT0xZYVFaNWFndk9VMEhZWlFRR2llMzlRbC9ibUJjUXJvdVdOanhtUldmMkIyNHBOK0ZMZEdZR0QiLCJtYWMiOiIyYjU5MWQxZTFiZDU1MzA3MWFmYmNkOGRmOTIzNGJjZDc5NzljMGE0OGRlMjk5OGJkMGE3NWI5OTBmNzliMjYwIiwidGFnIjoiIn0%3D'
             ),
             ));
@@ -163,17 +180,52 @@
                 $_SESSION['message_type'] = "error";
                 $_SESSION['message'] = "No se obtuvo una respuesta";
                 //TODO: cambiar la redireccion a al pantalla correspondiente
-                header("Location:  home");
+                // header("Location: home");
                 exit;
             }
 
             //TODO: cambiar la redireccion a al pantalla correspondiente
-            header("Location:  home");
+            // header("Location: home");
 
         }
 
-        public function logout() {
-            
+        public function logout($email) {
+
+            $curl = curl_init();
+
+            $data = $_SESSION['data'];
+
+            curl_setopt_array($curl, array(
+                CURLOPT_URL => 'https://crud.jonathansoto.mx/api/logout',
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_ENCODING => '',
+                CURLOPT_MAXREDIRS => 10,
+                CURLOPT_TIMEOUT => 0,
+                CURLOPT_FOLLOWLOCATION => true,
+                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                CURLOPT_CUSTOMREQUEST => 'POST',
+                CURLOPT_POSTFIELDS => array('email' => $email),
+                CURLOPT_HTTPHEADER => array(
+                  'Authorization: '. $data['token'],
+                  'Cookie: XSRF-TOKEN=eyJpdiI6IjZMUFA2eW8wVHVSRnk2QnlXV2lwWnc9PSIsInZhbHVlIjoid1gwWERkVlFHZWNmSVBWTGJUOXVaNDRUTXdJblY3V1ZLY3d0OUM4d1hDVUR4SjhjcE1uVTRWVW9XdlJrbFAwMU9FNkc1cVNveDV5ZFpKRldlNWpXTUJmTm1ZdUZkK3JBdE9uSGd4dkkyVEJYbnBXVi9rSGJKQ0R1Yk45Z3FkYVMiLCJtYWMiOiIwY2E4N2IzNGQ0MDQyN2Y0ODE4ZWVhYTMyZjRiMjlhNDNjNzVjMzY4NmExZjdkNTZjNWVmMDQyZDdlZWUwOGFmIiwidGFnIjoiIn0%3D; apicrud_session=eyJpdiI6IlU5UDFCbVc4RUd4YVJBVGtFdDlGa1E9PSIsInZhbHVlIjoiL21oRVVFN25OYm1xY2p6b3pxZ2NpL3d4L3FYZURmVk1wS2xIdlVJWFB3dHlwV1lJWVJURjc3cjJLWmdsN3J2OWR2SHRwOE9xdjdQeDRXVE5YZXVxNW1haTI4UkgrOER2QUgyMzFQQ25qZGhVbFVTdmZxSjBzb2pPZml4YVVQZFIiLCJtYWMiOiJmMDIyYzFmMjk3OWU3ZWRjYTI0MmJjOTFlMWI3MTJiMDgyMWM1YTVkOTVjYWE2ZjkwMzJkMzMzYTJkMzliNTc1IiwidGFnIjoiIn0%3D'
+                ),
+              ));
+
+            $response = curl_exec($curl);
+
+            curl_close($curl);
+
+            $response = json_decode($response, false);
+
+            if(isset($response) && $response->code == 2){
+                $_SESSION['message_type'] = "success";
+            } else {
+                $_SESSION['message_type'] = "error";
+            }
+            $_SESSION['message'] = $response->message;
+
+            //TODO: cambiar la redireccion a al pantalla correspondiente
+            // header("Location: home");
         }
     }
 
