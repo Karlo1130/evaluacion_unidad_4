2d00ef7188fb97837a7eb9a80c3b52ed70846db6
karlo <kaes_21@alu.uabcs.mx>
2024-11-12 23:14:32 -0700

    agregado update fun a ordersController

--- app/ordersController.php
+++ app/ordersController.php
@@ -36,7 +36,12 @@
                     break;
                         
                 case 'update':
+
+                    $id = $_POST['id'];
+                    $order_status_id = $_POST['order_status_id'];
                     
+                    $ordersController->update($id, $order_status_id);
+
                     break;
 
                 case "delete":
@@ -212,10 +217,6 @@
                 'coupon_id' => $coupon_id,
             ], $presentationsArray);
 
-            // $orderData = json_encode($orderData);
-
-            var_dump($orderData);
-
             $curl = curl_init();
 
             curl_setopt_array($curl, array(
@@ -257,8 +258,49 @@
             }
         }
             
-        function update() : void {
+        function update($id = null, $order_status_id = null) : void {
+            $sessionData = $_SESSION['data'];
+
+            $curl = curl_init();
+
+            curl_setopt_array($curl, array(
+            CURLOPT_URL => 'https://crud.jonathansoto.mx/api/orders',
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'PUT',
+            CURLOPT_POSTFIELDS => 'id='.$id.'&order_status_id='.$order_status_id,
+            CURLOPT_HTTPHEADER => array(
+                'Content-Type: application/x-www-form-urlencoded',
+                'Authorization: Bearer '.$sessionData->token,
+                'Cookie: XSRF-TOKEN=eyJpdiI6InRodXUwOHk1SnJXNUpLM3VTM2JEU2c9PSIsInZhbHVlIjoiYXp1TTdlOTZDbzJRS25QS2YvdUdzS2hSOU5wMWQ1dFpxbkd3SHFDUXJLckxMVHplenFPUTJHdjNIMzR1a1FLNlU1YzFPUFRZS3hJYjYwR2YxaWZsV25jZTA0dWtQdDVQTGZjUGZrSkU4Q2hFb05wRjNnR0pTZ2F6V3JkblI4NGIiLCJtYWMiOiI1NzAzYmY0YjU5NGRhZjBhODE3MmE4YjI2MGNlOWQ4ZTEyNTY2YTgwNzI5ZWQ4YjJmNWUwNmE3Y2RmNTk0NzA4IiwidGFnIjoiIn0%3D; apicrud_session=eyJpdiI6IkwxL2laalFYWFNSTUhrU1Y3cUh3a0E9PSIsInZhbHVlIjoiS0ZnOW5xeDBib29iNWZEYmxJZGhZM1kzNzlYOVh1U0pjWmtmUUkvcGhnckRwbnFNQUE0Mm96OHFUUVppbmxTZDhHOTJUSHZZNVFad2E5eWQyTTVqZktBVTAvcWNNbmhWejFSaFpLdWNXV1BRbk5mRzBaeTlOUENkT1UyTGZqYmMiLCJtYWMiOiIxMWMyNjAwYTkwZDViODNmMjNkNDYyYmIyMzk2ODQwMGViODRmMTczMmQ2ZTZjNmQyZjliZTcxZTliZGEwNTRhIiwidGFnIjoiIn0%3D'
+            ),
+            ));
+
+            $response = curl_exec($curl);
+
+            curl_close($curl);
+
+            if (!$response) {
+                $_SESSION['message_type'] = "error";
+                $_SESSION['message'] = "no se obtuvo una respuesta";
+                exit;
+            }
             
+            $response = json_decode($response, false);
+
+            var_dump($response);
+
+            if ($response->code == 4) {
+                $_SESSION['message_type'] = "success";
+                $_SESSION['message'] = $response->message;
+            } else {
+                $_SESSION['message_type'] = "error";
+                $_SESSION['message'] = $response->message;
+            }
         }
 
         function delete() : void {
