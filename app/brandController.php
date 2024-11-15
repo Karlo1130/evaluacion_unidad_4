df0fec227cf981b65ac6be50f549ef21b1512809
karlo <kaes_21@alu.uabcs.mx>
2024-11-15 02:07:10 -0700

    se agregan mensajes de control y se arregla error al obtener el token de session en brandController

--- app/brandController.php
+++ app/brandController.php
@@ -23,7 +23,7 @@
             CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
             CURLOPT_CUSTOMREQUEST => 'GET',
             CURLOPT_HTTPHEADER => array(
-                'Authorization: Bearer '.$sessionData['token'],
+                'Authorization: Bearer '.$sessionData->token,
                 'Cookie: XSRF-TOKEN=eyJpdiI6InJZNVNoQ2VtQ3lIdmR3bXRPdk54VFE9PSIsInZhbHVlIjoiVFhleEdzZ3BFclJiR1pNTFIyUHJuMHVEN0lNQm5tVi81aEYvL1JSV2JHT0N1WnorcStsOTNRcUFldVFGd0w3bmRIQXdJZGtQaWgrcmxndUhPV1FDejhUak54UjBPdnBqZktIWDZGNXVIOVFZN0hmenlyUE96cUxudmVUNVQza3IiLCJtYWMiOiJlY2I2MTIyMmY4YmI2NTVmMDgyMjcwNDI2MTg0Yzc0NzA3ZGIyMjk5M2M4YjI0MmIzNzFmZTIyMjAzMzk3MDZjIiwidGFnIjoiIn0%3D; apicrud_session=eyJpdiI6Ik9NTThTbWNjVzA5aHUwK3hQaGtiRWc9PSIsInZhbHVlIjoibExuUWhHZWNFdlJQQXVOamFNUVR2VjZueWwzMGVYOVRTMTBKemQrMzVUL3BOVHBsTnlTeU14UmlsUWNVMDhkUXExbUdJMHNKa1M0UU1JV2dXSlVOVDhLRmd6ZkFLQ0dKWk14VkdhanB5UmE5N05mdlZQa1NOd2wwVXVjdmJjMC8iLCJtYWMiOiJkNGQ3NTI3YjMxOTk0OGQ0NWE5ZGVlODExZjFiN2ZhYTZlNTIxMTcxNDg1ZGNkN2UyM2ZhZTJjMjE3NmFkMjA0IiwidGFnIjoiIn0%3D'
             ),
             ));
@@ -32,10 +32,27 @@
 
             curl_close($curl);
             
-            $response = json_decode($response, true);
-            $data = $response['data'];
+            if (!$response) {
+                $_SESSION['message_type'] = "error";
+                $_SESSION['message'] = "no se obtuvo una respuesta";
+                exit;
+            }
+            
+            $response = json_decode($response, false);
 
-            return $data;
+            if ($response->code == 4) {
+                $_SESSION['message_type'] = "success";
+                $_SESSION['message'] = $response->message;
+            } else {
+                $_SESSION['message_type'] = "error";
+                $_SESSION['message'] = $response->message;
+            }
+
+            if (is_array($response->data)) {
+                return $response->data;
+            } else {
+                return [];
+            }
         }
 
     }
