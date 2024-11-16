b04c92a89ecdff91c0670daa4846a928d653f223
karlo <kaes_21@alu.uabcs.mx>
2024-11-15 22:06:10 -0700

    se agrega get y getSpecificTags fun en tagsController

--- app/tagsController.php
+++ app/tagsController.php
@@ -5,8 +5,8 @@
     }
     $tagsController = new TagsController();
     if(isset($_POST["action"])) {
-        // if(isset($_POST['global_token']) 
-        //     && $_POST['global_token'] == $_SESSION['global_token']){
+        if(isset($_POST['global_token']) 
+            && $_POST['global_token'] == $_SESSION['global_token']){
             switch($_POST["action"]){
                 case 'get':
                     var_dump($levelsController->get());
@@ -15,14 +15,98 @@
                     var_dump($levelsController->getSpecificTag());
                     break;  
             }
-        // }
+        }
     }
     class TagsController {
         function get() : array {
+            $sessionData = $_SESSION['data'];
+
+            $curl = curl_init();
+
+            curl_setopt_array($curl, array(
+                CURLOPT_URL => 'https://crud.jonathansoto.mx/api/tags',
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_ENCODING => '',
+                CURLOPT_MAXREDIRS => 10,
+                CURLOPT_TIMEOUT => 0,
+                CURLOPT_FOLLOWLOCATION => true,
+                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                CURLOPT_CUSTOMREQUEST => 'GET',
+                CURLOPT_HTTPHEADER => array(
+                  'Authorization: Bearer '.$sessionData->token
+                ),
+              ));
+
+            $response = curl_exec($curl);
+
+            curl_close($curl);
+
+            if (!$response) {
+                $_SESSION['message_type'] = "error";
+                $_SESSION['message'] = "no se obtuvo una respuesta";
+                exit;
+            }
             
+            $response = json_decode($response, false);
+
+            if ($response->code == 4) {
+                $_SESSION['message_type'] = "success";
+                $_SESSION['message'] = $response->message;
+            } else {
+                $_SESSION['message_type'] = "error";
+                $_SESSION['message'] = $response->message;
+            }
+
+            if (is_array($response->data)) {
+                return $response->data;
+            } else {
+                return [];
+            }
         }
-        function getSpecificTag() : object {
+        function getSpecificTag($id = null) : object {
+            $sessionData = $_SESSION['data'];
+
+            $curl = curl_init();
+
+            curl_setopt_array($curl, array(
+                CURLOPT_URL => 'https://crud.jonathansoto.mx/api/tags/'.$id,
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_ENCODING => '',
+                CURLOPT_MAXREDIRS => 10,
+                CURLOPT_TIMEOUT => 0,
+                CURLOPT_FOLLOWLOCATION => true,
+                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                CURLOPT_CUSTOMREQUEST => 'GET',
+                CURLOPT_HTTPHEADER => array(
+                  'Authorization: Bearer '.$sessionData->token
+                ),
+              ));
+
+            $response = curl_exec($curl);
+
+            curl_close($curl);
+
+            if (!$response) {
+                $_SESSION['message_type'] = "error";
+                $_SESSION['message'] = "no se obtuvo una respuesta";
+                exit;
+            }
             
+            $response = json_decode($response, false);
+
+            if ($response->code == 4) {
+                $_SESSION['message_type'] = "success";
+                $_SESSION['message'] = $response->message;
+            } else {
+                $_SESSION['message_type'] = "error";
+                $_SESSION['message'] = $response->message;
+            }
+
+            if (is_object($response->data)) {
+                return $response->data;
+            } else {
+                return new stdClass();
+            }
         }
     }
 ?>
