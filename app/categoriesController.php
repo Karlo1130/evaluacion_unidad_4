037e6caea1d64ae864a2f362aa682fcb46735b4d
karlo <kaes_21@alu.uabcs.mx>
2024-11-15 21:58:52 -0700

    se agrega get y getSpecificCategory fun en categoriesController

--- app/categoriesController.php
+++ app/categoriesController.php
@@ -5,8 +5,8 @@
     }
     $categoriesController = new CategoriesController();
     if(isset($_POST["action"])) {
-        // if(isset($_POST['global_token']) 
-        //     && $_POST['global_token'] == $_SESSION['global_token']){
+        if(isset($_POST['global_token']) 
+            && $_POST['global_token'] == $_SESSION['global_token']){
             switch($_POST["action"]){
                 case 'get':
                     var_dump($levelsController->get());
@@ -15,14 +15,99 @@
                     var_dump($levelsController->getSpecificCategory());
                     break;  
             }
-        // }
+        }
     }
     class CategoriesController {
         function get() : array {
+            $sessionData = $_SESSION['data'];
+
+            $curl = curl_init();
+
+            curl_setopt_array($curl, array(
+                CURLOPT_URL => 'https://crud.jonathansoto.mx/api/categories',
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_ENCODING => '',
+                CURLOPT_MAXREDIRS => 10,
+                CURLOPT_TIMEOUT => 0,
+                CURLOPT_FOLLOWLOCATION => true,
+                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                CURLOPT_CUSTOMREQUEST => 'GET',
+                CURLOPT_HTTPHEADER => array(
+                  'Authorization: Bearer '.$sessionData->token
+                ),
+              ));
+
+            $response = curl_exec($curl);
+
+            curl_close($curl);
+
+            if (!$response) {
+                $_SESSION['message_type'] = "error";
+                $_SESSION['message'] = "no se obtuvo una respuesta";
+                exit;
+            }
             
+            $response = json_decode($response, false);
+
+            if ($response->code == 4) {
+                $_SESSION['message_type'] = "success";
+                $_SESSION['message'] = $response->message;
+            } else {
+                $_SESSION['message_type'] = "error";
+                $_SESSION['message'] = $response->message;
+            }
+
+            if (is_array($response->data)) {
+                return $response->data;
+            } else {
+                return [];
+            }
         }
-        function getSpecificCategory() : object {
+
+        function getSpecificCategory($id = null) : object {
+            $sessionData = $_SESSION['data'];
+
+            $curl = curl_init();
+
+            curl_setopt_array($curl, array(
+                CURLOPT_URL => 'https://crud.jonathansoto.mx/api/categories/'.$id,
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_ENCODING => '',
+                CURLOPT_MAXREDIRS => 10,
+                CURLOPT_TIMEOUT => 0,
+                CURLOPT_FOLLOWLOCATION => true,
+                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                CURLOPT_CUSTOMREQUEST => 'GET',
+                CURLOPT_HTTPHEADER => array(
+                  'Authorization: Bearer '.$sessionData->token
+                ),
+              ));
+
+            $response = curl_exec($curl);
+
+            curl_close($curl);
+
+            if (!$response) {
+                $_SESSION['message_type'] = "error";
+                $_SESSION['message'] = "no se obtuvo una respuesta";
+                exit;
+            }
             
+            $response = json_decode($response, false);
+
+            if ($response->code == 4) {
+                $_SESSION['message_type'] = "success";
+                $_SESSION['message'] = $response->message;
+            } else {
+                $_SESSION['message_type'] = "error";
+                $_SESSION['message'] = $response->message;
+            }
+
+            if (is_object($response->data)) {
+                return $response->data;
+            } else {
+                return new stdClass();
+            }
         }
 
     }
